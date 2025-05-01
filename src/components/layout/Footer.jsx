b3a84b12da81042f72a243039eebf50580537ad1
momarzouq <smsabomarzoq@gmail.com>
2025-05-01 09:17:53 +0200

    Finish Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,12 +1,12 @@
-import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTelegramPlane } from "react-icons/fa";
 import Container from "../../UI/Container";
 import Payment from "../../assets/Payment_footer.svg";
 import Logo from "../../assets/logo.svg";
 export default function Footer() {
   return (
-    <div className="bg-black py-16">
+    <div className="bg-black pt-14 pb-10">
       <Container>
-        <div className="flex gap-28">
+        <div className="flex gap-40 xl:gap-32 md:flex-col md:items-start">
           {/* Products */}
           <div className="flex flex-col text-gray-300 space-y-4">
             <p className="text-white font-semibold">Products</p>
@@ -48,7 +48,7 @@ export default function Footer() {
               <FaLinkedinIn size={25} />
             </div>
 
-            <div className="flex flex-col gap-2 w-80">
+            <div className="relative flex flex-col gap-2 w-80">
               <label
                 htmlFor="email"
                 className="text-red-600 font-medium text-sm"
@@ -61,10 +61,13 @@ export default function Footer() {
                 placeholder="Email address"
                 className="text-sm border border-gray-300 rounded-sm p-2 focus:outline-none focus:ring-1 focus:ring-red-600"
               />
+              <FaTelegramPlane className="absolute right-4 top-9 text-red-600 size-6"/>
             </div>
           </div>
         </div>
       </Container>
+      <hr className="mt-6 border-1 border-gray-700" />
+      <p className="text-sm text-center text-white mt-8">Copyright © 2025 O2Mart. All Rights Reserved</p>
     </div>
   );
 }
