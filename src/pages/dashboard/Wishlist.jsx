3239f66cb637f1f8059a6b086953b65ab04d767c
momarzouq <smsabomarzoq@gmail.com>
2025-05-06 07:45:03 +0200

    edit card of products

--- src/pages/dashboard/Wishlist.jsx
+++ src/pages/dashboard/Wishlist.jsx
@@ -56,61 +56,6 @@ const Wishlist = () => {
       rating: 5,
       brand: Nissan,
     },
-    {
-      id: 5,
-      name: "Mazda Motor Oil SN 5W-30 Golden",
-      price: 165,
-      originalPrice: 179,
-      sale: true,
-      volume: Volum,
-      image: Tyers,
-      rating: 5,
-      brand: Nissan,
-    },
-    {
-      id: 6,
-      name: "Mazda Motor Oil SN 5W-30 Golden",
-      price: 165,
-      originalPrice: 179,
-      sale: true,
-      volume: Volum,
-      image: Tyers,
-      rating: 5,
-      brand: Nissan,
-    },
-    {
-      id: 7,
-      name: "Mazda Motor Oil SN 5W-30 Golden",
-      price: 165,
-      originalPrice: 179,
-      sale: false,
-      volume: Volum,
-      image: Tyers,
-      rating: 5,
-      brand: Nissan,
-    },
-    {
-      id: 8,
-      name: "Mazda Motor Oil SN 5W-30 Golden",
-      price: 165,
-      originalPrice: 179,
-      sale: true,
-      volume: Volum,
-      image: Tyers,
-      rating: 5,
-      brand: Nissan,
-    },
-    {
-      id: 9,
-      name: "Mazda Motor Oil SN 5W-30 Golden",
-      price: 165,
-      originalPrice: 179,
-      sale: true,
-      volume: Volum,
-      image: Tyers,
-      rating: 5,
-      brand: Nissan,
-    },
   ];
 
   return (
@@ -124,63 +69,65 @@ const Wishlist = () => {
 
       <div className="flex gap-6 md:flex-col">
         <DashboardSideNav />
-        <div className="grid grid-cols-2 md:grid-cols-1 gap-6">
+        <div className="grid grid-cols-3 gap-4">
           {TiresProducts.map((product) => (
-            <Link
+            <div
               key={product.id}
-              to={`/product/${product.id}`}
-              className="relative flex flex-col items-center justify-center border p-4 hover:shadow-lg"
+              className="relative flex flex-col items-center justify-center border p-4 space-y-2"
             >
-              <div className="relative flex flex-col items-center justify-center p-4 ">
-                {/* Brand Of Product & Add To Favorit & Comprison */}
-                <div className="flex items-center">
-                  <img
-                    src={product.brand}
-                    alt={product.brand}
-                    className="absolute left-4 top-2 object-cover rounded h-12"
-                  />
-                  <IoHeartOutline className="absolute right-4 top-2 size-6" />
-                  <MdOutlineCompareArrows className="absolute right-11 top-2 size-6" />
-                </div>
+              {/* Brand Of Product & Add To Favorit & Comprison */}
+              <div className=" flex items-center">
+                <img
+                  src={product.brand}
+                  alt={product.brand}
+                  className="absolute left-4 top-2 object-cover rounded h-12"
+                />
+                <IoHeartOutline className="absolute right-4 top-2 size-6 hover:text-Brand" />
+                <Link to="/comparsion">
+                  <MdOutlineCompareArrows className="absolute right-11 top-2 size-6 hover:text-Brand" />
+                </Link>
+              </div>
+              <Link to={`/product/${product.id}`}>
                 <img
                   src={product.image}
                   alt={product.name}
-                  className="object-cover rounded h-20 my-6"
+                  className="object-cover rounded h-20 my-6 flex justify-center"
                 />
-                <img
-                  src={product.volume}
-                  alt={product.name}
-                  className="object-cover"
-                />
-                <h3 className="mt-2 font-semibold w-48 text-center">
-                  {product.name}
-                </h3>
-                <div className="flex items-center gap-1 mt-1">
-                  {renderStars(product.rating)}
-                </div>
-                <div className="mt-2 flex items-center gap-2">
-                  {product.originalPrice && (
-                    <span className="line-through text-sm text-gray-400">
-                      AED {product.originalPrice}
-                    </span>
-                  )}
-                  <span className="font-semibold">
-                    <span className="text-gray-400">AED</span> ${product.price}
-                  </span>
-                </div>
-                <div className="relative">
-                  <select className="absolute border outline-none rounded-2xl py-[2px] md:py-0 px-4">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                  </select>
-                  <button className=" text-sm text-gray-500 border border-Brand rounded-2xl py-1 md:py-0.5 px-20 xl:px-10">
-                    Add to Cart
-                  </button>
-                </div>
+              </Link>
+              <img
+                src={product.volume}
+                alt={product.name}
+                className="object-cover"
+              />
+              <h3 className="mt-2 font-semibold w-48 text-center">
+                {product.name}
+              </h3>
+              <div className="flex items-center gap-1 mt-1">
+                {renderStars(product.rating)}
               </div>
-            </Link>
+              <div className="mt-2 flex items-center gap-2">
+                {product.originalPrice && (
+                  <span className="line-through text-sm text-gray-400">
+                    AED {product.originalPrice}
+                  </span>
+                )}
+                <span className="font-semibold">
+                  <span className="text-gray-400">AED</span> ${product.price}
+                </span>
+              </div>
+
+              <div className="relative">
+                <select className="absolute border outline-none rounded-2xl py-[2px] md:py-0 px-4">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                </select>
+                <button className=" text-sm text-gray-500 border border-Brand rounded-2xl py-1 md:py-0.5 px-20 xl:px-10">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
           ))}
         </div>
       </div>
