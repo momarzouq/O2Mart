84c93c609d4665e57f3603a5e3340aef72e896c8
momarzouq <smsabomarzoq@gmail.com>
2025-05-09 21:43:57 +0200

    fix error  svg

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,  // Set the port to 3000
+    port: 3000, // Set the port to 3000
   },
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr(), // Add SVGR plugin for SVG support
+  ],
+});
