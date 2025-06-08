3e0f3b269a4588adf3fd9acbfbcd8d918a33529e
momarzouq <smsabomarzoq@gmail.com>
2025-06-08 13:57:13 +0200

    remove all rights

--- src/components/LocationModal.jsx
+++ src/components/LocationModal.jsx
@@ -7,6 +7,7 @@ import {
 import { BsClock } from "react-icons/bs";
 import { SlClose } from "react-icons/sl";
 import { Link } from "react-router-dom";
+import { dates } from "../constants/locations";
 
 export const LocationModal = ({ selectedOption,onClose  }) => {
   const [currentCase, setCurrentCase] = useState(1);
@@ -81,19 +82,6 @@ export const LocationModal = ({ selectedOption,onClose  }) => {
     },
   ];
 
-  const dates = [
-    { date: "Today 21 Mar", available: false },
-    { date: "SAT 22 Mar", available: true },
-    { date: "SUN 23 Mar", available: false },
-    { date: "MON 24 Mar", available: true },
-    { date: "TUS 23 Mar", available: true },
-    { date: "WED 24 Mar", available: true },
-    { date: "THU 23 Mar", available: true },
-    { date: "SAT 24 Mar", available: true },
-    { date: "SUN 24 Mar", available: false },
-    { date: "SAT 24 Mar", available: true },
-    { date: "SUN 24 Mar", available: false },
-  ];
   const indexOfLastDate = currentPage * datesPerPage;
   const indexOfFirstDate = indexOfLastDate - datesPerPage;
   const currentDates = dates.slice(indexOfFirstDate, indexOfLastDate);
---
+++ src/constants/locations.js
@@ -0,0 +1,13 @@
+export const dates = [
+  { date: "Today 21 Mar", available: false },
+  { date: "SAT 22 Mar", available: true },
+  { date: "SUN 23 Mar", available: false },
+  { date: "MON 24 Mar", available: true },
+  { date: "TUS 23 Mar", available: true },
+  { date: "WED 24 Mar", available: true },
+  { date: "THU 23 Mar", available: true },
+  { date: "SAT 24 Mar", available: true },
+  { date: "SUN 24 Mar", available: false },
+  { date: "SAT 24 Mar", available: true },
+  { date: "SUN 24 Mar", available: false },
+];
